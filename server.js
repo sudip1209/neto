0dec93fdf345b1255e84dd0234c7f5745dc9bcc8
Sudip Sharma <130955377+sudip1209@users.noreply.github.com>
2025-08-16 21:55:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
 
+// dotenv.config({ path: "./config.env" });
+// const app = require("./app");
+
+// const DB = process.env.DATABASE.replace(
+//   "<PASSWORD>",
+//   process.env.DATABASE_PASSWORD
+// );
+
+// // Connect to MongoDB
+// mongoose
+//   .connect(DB, {})
+//   .then(() => console.log("DB connection successful!"))
+//   .catch((err) => {
+//     console.error("DB connection error:", err.message);
+//     process.exit(1);
+//   });
+
+// // 🚨 Do NOT call app.listen here for Vercel
+// // Just export the app
+// module.exports = app;
+
+
+const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
+
 const app = require("./app");
 
-const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
-  process.env.DATABASE_PASSWORD
-);
-
-// Connect to MongoDB
-mongoose
-  .connect(DB, {})
-  .then(() => console.log("DB connection successful!"))
-  .catch((err) => {
-    console.error("DB connection error:", err.message);
-    process.exit(1);
-  });
-
-// 🚨 Do NOT call app.listen here for Vercel
-// Just export the app
+// 🚨 No DB connection here
 module.exports = app;
+
